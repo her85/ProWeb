902dee386996fefadfe4ff0b71bc27930eb5f914
HerHub <hab_85@live.com.ar>
2023-06-09 11:52:18 -0300

    r

--- eventos.js
+++ eventos.js
@@ -25,8 +25,9 @@ function validar(e) {
     } else if (email == "") {
         document.querySelector("#email").focus();
     } else {
-        sueldoEuro = sueldo / 259;
-        sueldoDolar = sueldo / 251;
+        sueldo = parseInt(sueldo);
+        sueldoEuro = parseInt(sueldo / 259);
+        sueldoDolar = parseInt(sueldo / 251);
 
 
         if (sueldo < 100000) {
@@ -35,7 +36,6 @@ function validar(e) {
         } else {
             oferta = { nombre, email, telefono, sueldo, sueldoEuro, sueldoDolar };
             ofertas.push(oferta);
-
             llenarTabla();
         }
 
@@ -43,6 +43,7 @@ function validar(e) {
         email = document.querySelector("#email").value = "";
         telefono = document.querySelector("#telefono").value = "";
         sueldo = document.querySelector("#sueldo").value = "";
+        texto = document.querySelector("#texto").value = "";
         document.querySelector("#nombre").focus();
     }
 
@@ -53,8 +54,6 @@ function llenarTabla() {
     tableBody = document.querySelector("#respuestas");
     tableBody.innerHTML = "";
 
-
-    // Resto del código de llenado de la tabla
     ofertas.forEach(dato => {
         fila = document.createElement("tr");
         nombreColumna = document.createElement("td");
@@ -67,9 +66,9 @@ function llenarTabla() {
         nombreColumna.textContent = dato.nombre;
         emailColumna.textContent = dato.email;
         telefonoColumna.textContent = dato.telefono;
-        sueldoColumna.textContent = dato.sueldo;
-        sueldoEuroColumna.textContent = dato.sueldoEuro;
-        sueldoDolarColumna.textContent = dato.sueldoDolar;
+        sueldoColumna.textContent = "$" + dato.sueldo;
+        sueldoEuroColumna.textContent = "€" + dato.sueldoEuro;
+        sueldoDolarColumna.textContent = "US$" + dato.sueldoDolar;
 
 
         fila.appendChild(nombreColumna);
@@ -80,40 +79,26 @@ function llenarTabla() {
         fila.appendChild(sueldoDolarColumna);
         tableBody.appendChild(fila);
     });
-
 }
 
-// Obtener referencias a las columnas de encabezado
-columnaNombre = document.querySelector("#nombre-header");
-columnaEmail = document.querySelector("#email-header");
-columnaTelefono = document.querySelector("#telefono-header");
 columnaSueldo = document.querySelector("#sueldo-header");
-columnaSueldoEuro = document.querySelector("#sueldoEuro-header");
-columnaSueldoDolar = document.querySelector("#sueldoDolar-header");
-
-// Agregar eventos de clic a las columnas de encabezado
-columnaNombre.addEventListener("click", () => ordenarTabla("nombre"));
-columnaEmail.addEventListener("click", () => ordenarTabla("email"));
-columnaTelefono.addEventListener("click", () => ordenarTabla("telefono"));
 columnaSueldo.addEventListener("click", () => ordenarTabla("sueldo"));
-columnaSueldoEuro.addEventListener("click", () => ordenarTabla("sueldoEuro"));
-columnaSueldoDolar.addEventListener("click", () => ordenarTabla("sueldoDolar"));
 
-columnaActual = ""; // Variable para almacenar la columna actualmente ordenada
-ordenAscendente = true; // Variable para almacenar el orden actual (ascendente o descendente)
+columnaActual = "";
+ordenAscendente = true;
 
 function ordenarTabla(columna) {
-    // Verificar si se está ordenando por la misma columna
+
     if (columna === columnaActual) {
-        // Cambiar el orden de ascendente a descendente y viceversa
+
         ordenAscendente = !ordenAscendente;
     } else {
-        // Establecer la nueva columna de ordenamiento y el orden ascendente
+
         columnaActual = columna;
         ordenAscendente = true;
     }
 
-    // Ordenar la tabla según la columna seleccionada y el orden actual
+
     ofertas.sort((a, b) => {
         if (a[columna] < b[columna]) {
             return ordenAscendente ? -1 : 1;
